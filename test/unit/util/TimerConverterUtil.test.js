fa3bdb484b5e7bebab5d831e1cecc57204fd37bb
alvarengacarlos <carlosho200@hotmail.com>
2023-03-02 20:58:36 -0400

    feat: Creates the logger and timer converter util

---
+++ test/unit/util/TimerConverterUtil.test.js
@@ -0,0 +1,41 @@
+const TimerConverterUtil = require('../../../lib/util/TimerConverterUtil')
+
+describe('TimerConverterUtil', () => {
+
+    describe('millisecondsToSeconds', () => {
+
+        test('should convert the milliseconds to seconds with success', () => {
+            expect(TimerConverterUtil.millisecondsToSeconds(1000)).toBe(1)
+            expect(TimerConverterUtil.millisecondsToSeconds(2000)).toBe(2)
+        })
+
+    })
+
+    describe('secondsToMilliseconds', () => {
+
+        test('should convert the seconds to milliseconds with success', () => {
+            expect(TimerConverterUtil.secondsToMilliseconds(1)).toBe(1000)
+            expect(TimerConverterUtil.secondsToMilliseconds(2)).toBe(2000)
+        })
+
+    })
+
+    describe('minutesToSeconds', () => {
+
+        test('should convert the minutes to seconds with success', () => {
+            expect(TimerConverterUtil.minutesToSeconds(1)).toBe(60)
+            expect(TimerConverterUtil.minutesToSeconds(2)).toBe(120)
+        })
+
+    })
+
+    describe('secondsToMinutes', () => {
+
+        test('should convert the seconds to minutes with success', () => {
+            expect(TimerConverterUtil.secondsToMinutes(60)).toBe(1)
+            expect(TimerConverterUtil.secondsToMinutes(120)).toBe(2)
+        })
+
+    })
+
+})
